3b015abd55e34cd04af954533f5f09ab1cc571bc
Md. Shakil Alam <44320119+Shakil-Alam76@users.noreply.github.com>
2022-08-03 00:47:25 +0600

    "Added ifElement Exists And writeOnTextBox"

--- src/test/java/TestSeleniumJUnit.java
+++ src/test/java/TestSeleniumJUnit.java
@@ -27,6 +27,27 @@ public class TestSeleniumJUnit {
      Assertions.assertTrue(title.contains("ToolsQA"));
      System.out.println("Test Sucsessfully Done");
     }
+ 
+ @Test
+    public void ifElementExists(){
+        driver.get("https://demoqa.com");
+        wait=new WebDriverWait(driver,20);
+        Boolean status=wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//img[@src='/images/Toolsqa.jpg']"))).isDisplayed();
+        // Boolean status= driver.findElement(By.xpath("//img[@src='/images/Toolsqa.jpg']")).isDisplayed();
+
+        Assertions.assertEquals(status,true);
+    }
+@Test
+    public void writeOnTextBox(){
+        driver.get("https://demoqa.com/elements");
+        driver.findElement(By.xpath("//span[contains(text(),'Text Box')]")).click();
+        wait=new WebDriverWait(driver,30);
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("userName"))).sendKeys("Shanto");
+        driver.findElement(By.cssSelector("#submit")).click();
+        String text=driver.findElement(By.cssSelector("id=name")).getText();
+        Assertions.assertTrue(text.contains("Shanto"));
+    }
+ 
     @AfterAll
     public static void finishTest(){
         driver.close();
